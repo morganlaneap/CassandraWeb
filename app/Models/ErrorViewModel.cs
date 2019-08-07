d2f8a0a8b992a4916ab7ab39d39f32a0ad832d7a
Morgan Lane <morgan.lane@redditchconnected.co.uk>
2019-08-07 15:29:56 +0100

    Initial commit

---
+++ app/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CassandraWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
