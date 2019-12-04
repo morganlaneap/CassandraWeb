529cafd789d60e61818986b3807bd8600ad4d9f0
Morgan Lane <morganlane@bristowsutor.co.uk>
2019-12-04 08:18:19 +0000

    Refactoring of folder structure

--- src/wwwroot/angular/app.states.js
+++ src/wwwroot/angular/app.states.js
@@ -1,42 +1,41 @@
 cassandraWeb.config(function($stateProvider) {
-    $stateProvider.state('dashboard', {
-      url: '',
-      views: {
-        'content': {
-          templateUrl: '/angular/dashboard/dashboard.html',
-          controller: 'dashboardController',
-        }
+  $stateProvider.state("dashboard", {
+    url: "",
+    views: {
+      content: {
+        templateUrl: "/angular/dashboard/dashboard.html",
+        controller: "dashboardController"
       }
-    });
-
-    $stateProvider.state('connection', {
-      url: '/connection/:connectionId',
-      views: {
-        'content': {
-          templateUrl: '/angular/connection/connection.html',
-          controller: 'connectionController',
-        }
-      }
-    });
-
-    $stateProvider.state('connectionKeyspaceTable', {
-      url: '/connection/:connectionId/keyspace/:keyspaceName/table/:tableName',
-      views: {
-        'content': {
-          templateUrl: '/angular/connection/table.html',
-          controller: 'connectionTableController',
-        }
-      }
-    });
-
-    $stateProvider.state('connectionKeyspaceNewTable', {
-      url: '/connection/:connectionId/keyspace/:keyspaceName/new-table',
-      views: {
-        'content': {
-          templateUrl: '/angular/connection/newTable.html',
-          controller: 'connectionNewTableController',
-        }
-      }
-    });
+    }
   });
-  
+
+  $stateProvider.state("connection", {
+    url: "/connection/:connectionId",
+    views: {
+      content: {
+        templateUrl: "/angular/connection/connection.html",
+        controller: "connectionController"
+      }
+    }
+  });
+
+  $stateProvider.state("connectionKeyspaceTable", {
+    url: "/connection/:connectionId/keyspace/:keyspaceName/table/:tableName",
+    views: {
+      content: {
+        templateUrl: "/angular/connection/table/table.html",
+        controller: "connectionTableController"
+      }
+    }
+  });
+
+  $stateProvider.state("connectionKeyspaceNewTable", {
+    url: "/connection/:connectionId/keyspace/:keyspaceName/new-table",
+    views: {
+      content: {
+        templateUrl: "/angular/connection/newTable/newTable.html",
+        controller: "connectionNewTableController"
+      }
+    }
+  });
+});
